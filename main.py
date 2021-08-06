29f8e77b3f9db7ef9bf0c58b37e7ca8c6567bc6a
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-08-06 20:05:40 +0800

    finished writing server, will continue tomorrow

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"Greetings": "Welcome to FastAPI!"}
+
+@app.post("/add")
+async def add_entry(key: str, value):
+    hash_table.add(key, value)
+    return f"Key-value pair {key}-{value} added"
+
+@app.delete("/delete")
+async def delete_entry(key: str):
+    hash_table.delete(key)
+    return f"Key: {key}deleted"
+
+@app.get("/get")
+async def get_value(key: str):
+    return hash_table.get(key)
