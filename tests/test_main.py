43ca15d1c071d40bfd6fab70e4a7ded24c7e6402
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-08-07 12:50:01 +0800

    added test for get method

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,3 +15,10 @@ def test_post_method_add():
     response = client.post(f"/add/{key}-{value}")
     assert response.status_code == 200
     assert response.json() == f"Key-value pair {key}-{value} added"
+
+def test_get_method_get():
+    key = "FastAPI"
+    expected_value = "tutorial"
+    response = client.get(f"/get/{key}")
+    assert response.status_code == 200
+    assert response.json() == expected_value
