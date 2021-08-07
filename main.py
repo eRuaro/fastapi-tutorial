7f654ed077ccbf22a2e8343d1db9507a5344f1fc
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-08-07 11:25:44 +0800

    about to write test

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ async def add_entry(key: str, value):
     hash_table.add(key, value)
     return f"Key-value pair {key}-{value} added"
 
-# @app.delete("/delete")
-# async def delete_entry(key: str):
-#     hash_table.delete(key)
-#     return f"Key: {key}deleted"
+@app.delete("/delete/{key}")
+async def delete_entry(key: str):
+    hash_table.delete(key)
+    return f"Key: {key}deleted"
 
-# @app.get("/get")
-# async def get_value(key: str):
-#     return hash_table.get(key)
+@app.get("/get/{key}")
+async def get_value(key: str):
+    return hash_table.get(key)
